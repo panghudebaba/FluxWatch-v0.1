d6b9132400bef1565877ef4e8909d1fbdce48c2a
hjli <148666128+panghudebaba@users.noreply.github.com>
2026-03-05 09:58:11 +0800

    Update app.R

--- app.R
+++ app.R
@@ -1,8 +1,11 @@
 library(shiny)
 library(shinydashboard)
 
+# 直接加载 ui.R 和 server.R，而不需要再 source ui.R
 source("global.R", local = TRUE)
-# source("ui.R", local = TRUE)
 source("server.R", local = TRUE)
 
-shinyApp(ui, server)
+# ui 应该在此引用 ui.R 中的对象
+shinyApp(ui = ui, server = server)
+
+
