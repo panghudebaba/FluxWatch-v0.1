04583f55cea5f35b38996445df1bb7b817372e99
hjli <148666128+panghudebaba@users.noreply.github.com>
2026-03-05 09:56:22 +0800

    Add files via upload

--- app.R
+++ app.R
@@ -2,7 +2,7 @@ library(shiny)
 library(shinydashboard)
 
 source("global.R", local = TRUE)
-source("ui.R", local = TRUE)
+# source("ui.R", local = TRUE)
 source("server.R", local = TRUE)
 
 shinyApp(ui, server)
